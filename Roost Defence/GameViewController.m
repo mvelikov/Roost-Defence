3397604a2482d752a9e26e952731c45223c38195
Mihail Velikov <mihailvelik@gmail.com>
2014-05-06 16:09:07 +0300

    implement startGame and enemyMovement methods

--- Roost Defence/GameViewController.m
+++ Roost Defence/GameViewController.m
@@ -14,6 +14,33 @@
 
 @implementation GameViewController
 
+- (void) startGame
+{
+    towerOne.image = [UIImage imageNamed:EMPTY_TOWER];
+    towerTwo.image = [UIImage imageNamed:EMPTY_TOWER];
+    
+    enemy.center = CGPointMake(ROUTE_START_X, ROUTE_START_Y);
+    
+    gameEngine = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(enemyMovement) userInfo:nil repeats:YES];
+}
+- (void) endGame
+{
+    
+}
+- (void) enemyMovement
+{
+    if (enemy.center.x < ROUTE_LEFT_X && enemy.center.y == ROUTE_START_Y) {
+        enemy.center = CGPointMake(enemy.center.x + 1, ROUTE_START_Y);
+    } else if (enemy.center.x == ROUTE_LEFT_X && enemy.center.y > ROUTE_RIGHT_Y) {
+        enemy.center = CGPointMake(ROUTE_LEFT_X, enemy.center.y - 1);
+    } else if (enemy.center.x >= ROUTE_LEFT_X && enemy.center.y == ROUTE_RIGHT_Y) {
+        enemy.center = CGPointMake(enemy.center.x + 1, ROUTE_END_Y);
+    }
+}
+- (void) towerAttack
+{
+    
+}
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
