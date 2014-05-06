298db99ed38bb212ef6b118d9f98a40a81dea15f
Mihail Velikov <mihailvelik@gmail.com>
2014-05-06 15:23:15 +0300

    init

---
+++ Roost Defence/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Roost Defence
+//
+//  Created by mihata on 5/6/14.
+//  Copyright (c) 2014 mihata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Roost Defence/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Roost Defence
+//
+//  Created by mihata on 5/6/14.
+//  Copyright (c) 2014 mihata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
